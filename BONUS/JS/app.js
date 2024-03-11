6c08f7a8cbf67c398b69970845333733b098cac3
RaffaeleVallifuoco <ra.vallifuoco@outlook.it>
2024-03-11 18:38:23 +0100

    age selector error

--- BONUS/JS/app.js
+++ BONUS/JS/app.js
@@ -11,11 +11,12 @@ console.log('distanza :  ' +distanceElement);
 const ageElement = document.getElementById('age'); //string | null
 console.log('età ; ' +ageElement);
 
+
 submitElement.addEventListener('click', function() {
     console.log ('click sul submit')
     const distance = parseInt(distanceElement.value);
     console.log(distance);
-    const age = parseInt(ageElement.value);
+    const age = ageElement.option;
     console.log(age);
     let price_base = 0.21 * distance; //number
     price_base = price_base.toFixed(2);
@@ -28,7 +29,7 @@ submitElement.addEventListener('click', function() {
     price_discount = price_base;
     const ticketPrice = document.getElementById('price');
     ticketPrice.innerHTML = 'Prezzo del biglietto : ' + price_discount + ' €';
-    if (age < 18) {
+    if (ageElement.option ==under) {
         discount = price_base * 0.2;
         let price_discount = price_base - discount;
         console.log('Sconto 20% applicato');
