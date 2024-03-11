e71e182684a9a3d433ff6ab2bd9bc83f65a80e1a
RaffaeleVallifuoco <ra.vallifuoco@outlook.it>
2024-03-11 17:36:51 +0100

    creating js

--- JS/app.js
+++ JS/app.js
@@ -2,38 +2,56 @@ console.log ('console test');
 
 // Inserimento età e km da percorrere
 
-let distance = prompt('Inserisci i chilometri che desieri percorrere'); //string | null
-distance = parseInt(distance); //number | null
-console.log('distanza: ' +distance + ' km');
-let age = prompt('Inserisci la tua età'); //string | null
-age = parseInt(age).toFixed(2); //number ! null
-console.log('età: '+ age +' anni');
+// leggo elementi dom e ascolto click
+const submitElement = document.getElementById('submit'); //object
+
+const distanceElement = document.getElementById('distance'); //string | null
+console.log('distanza :  ' +distanceElement);
+
+const ageElement = document.getElementById('age'); //string | null
+console.log('età ; ' +ageElement);
+
+submitElement.addEventListener('click', function() {
+    console.log ('click sul submit')
+    const distance = parseInt(distanceElement.value);
+    console.log(distance);
+    const age = parseInt(ageElement.value);
+    console.log(age);
+    let price_base = 0.21 * distance; //number
+    price_base = price_base.toFixed(2);
+    console.log('Il prezzo base è : ' + price_base + ' €');
+
+
+    // calcolo buono sconto
+
+    let discount = 0; //number 
+    price_discount = price_base;
+    const ticketPrice = document.getElementById('price');
+    ticketPrice.innerHTML = 'Prezzo del biglietto : ' + price_discount + ' €';
+    if (age < 18) {
+        discount = price_base * 0.2;
+        let price_discount = price_base - discount;
+        console.log('Sconto 20% applicato');
+        console.log('Prezzo scontato : ' + price_discount + ' €');
+        const ticketPrice = document.getElementById('price');
+        ticketPrice.innerHTML = 'Prezzo del biglietto : ' + price_discount + ' €';
+    } else if (age >= 65) {
+        discount = price_base * 0.4;
+        let price_discount = price_base - discount;
+        console.log('Sconto 40% applicato');
+        console.log('Prezzo scontato : ' + price_discount + ' €');
+        const ticketPrice = document.getElementById('price');
+        ticketPrice.innerHTML = 'Prezzo del biglietto : ' + price_discount + ' €';
+    
+
+    
+    }
+    
+})
 
 
 // calcolo prezzo base del biglietto
 
-let price_base = 1.21*distance; //number
-price_base = price_base.toFixed(2);
-console.log ('Il prezzo base è : ' + price_base +' €');
 
-
-// calcolo buono sconto
-
-let discount = 0; //number 
-price_discount=price_base;
-if (age < 18) {
-    discount = price_base * 0.2;
-    let price_discount = price_base - discount;
-    console.log ('Sconto 20% applicato' );
-    console.log ('Prezzo scontato : ' +price_discount + ' €') ;
-} else if (age >= 65) {
-    discount = price_base * 0.4;
-    let price_discount = price_base - discount;
-    console.log('Sconto 40% applicato');
-    console.log('Prezzo scontato : ' + price_discount + ' €');
-}
-
-const ticketPrice = document.getElementById ('price');
-ticketPrice.innerHTML= 'Prezzo del biglietto : '+ price_discount;
 
             
